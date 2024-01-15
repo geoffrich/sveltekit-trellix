b6ca82f9e70d85e5093b4e14db843cb6c8d89e22
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2024-01-15 14:19:42 -0800

    Use $page.data type

--- src/routes/home/Boards.svelte
+++ src/routes/home/Boards.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import Board from './Board.svelte';
-	//import { PageData } from './$types' // TODO not working;
+	import type { PageData } from './$types';
+
+	$: boards = ($page.data as PageData).boards;
 
 	/*
-    notes: no useLoaderData, need to pass down prop or use $page.data
+    notes: no useLoaderData, can use $page.data and import $types from page
     */
 </script>
 
 <div class="p-8">
 	<h2 class="mb-2 text-xl font-bold">Boards</h2>
 	<nav class="flex flex-wrap gap-8">
-		{#each $page.data.boards as board (board.id)}
+		{#each boards as board (board.id)}
 			<Board name={board.name} id={board.id} color={board.color} />
 		{/each}
 	</nav>
